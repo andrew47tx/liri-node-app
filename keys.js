1e210e6d8d16c878ca058830dc207d029725290f
Andrew Robledo <andrewrobledo2@gmail.com>
2018-12-15 19:23:59 -0600

    completed js

--- keys.js
+++ keys.js
@@ -5,3 +5,9 @@ exports.spotify = {
   secret: process.env.SPOTIFY_SECRET
 };
 
+exports.twitterKeys = {
+    consumer_key: 'Zj8MzJTHwqfouq33dzWSWlX4L',
+    consumer_secret: '9GpctAfDK8OcvB1dQYWwK71deGQ6c7B0MKH6daiAI7DFHj4rop',
+    access_token_key: '849082256903876609-BFeWj2aYoOADXqiw0EHovigCjCATGPG',
+    access_token_secret: 'NVWJj0JhUJdehXWwHwIO12ynI4YIf45xKmtTDkArSx29A',
+  }
